a005cc1387bfe301a4c1db2a884c5f8ac5483bd7
Михаил Шелудяков <bvg2003@bk.ru>
2025-02-20 01:41:41 +0300

    AOP

--- src/main/java/ru/michaelshell/taskmanager/controller/TaskController.java
+++ src/main/java/ru/michaelshell/taskmanager/controller/TaskController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import ru.michaelshell.taskmanager.aop.annotation.LogAfterReturning;
+import ru.michaelshell.taskmanager.aop.annotation.LogAfterThrowing;
+import ru.michaelshell.taskmanager.aop.annotation.LogBefore;
+import ru.michaelshell.taskmanager.aop.annotation.LogExecutionTime;
 import ru.michaelshell.taskmanager.model.dto.CreateTaskRequest;
 import ru.michaelshell.taskmanager.model.dto.TaskDto;
 import ru.michaelshell.taskmanager.model.dto.UpdateTaskRequest;
@@ -26,27 +30,34 @@ public class TaskController {
 
     private final TaskService taskService;
 
+    @LogAfterReturning
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public TaskDto createTask(@RequestBody @Valid CreateTaskRequest createTaskRequest) {
         return taskService.createTask(createTaskRequest);
     }
 
+    @LogExecutionTime
     @GetMapping
     public List<TaskDto> getTasks() {
         return taskService.getAllTasks();
     }
 
+    @LogExecutionTime
+    @LogAfterThrowing
     @GetMapping("/{id}")
     public TaskDto getTask(@PathVariable Long id) {
         return taskService.getTask(id);
     }
 
+    @LogBefore
+    @LogAfterReturning
     @PutMapping("/{id}")
     public TaskDto updateTask(@PathVariable Long id, @RequestBody @Valid UpdateTaskRequest updateTaskRequest) {
         return taskService.updateTask(id, updateTaskRequest);
     }
 
+    @LogBefore
     @DeleteMapping("/{id}")
     public String deleteTask(@PathVariable Long id) {
         return taskService.deleteTask(id);
