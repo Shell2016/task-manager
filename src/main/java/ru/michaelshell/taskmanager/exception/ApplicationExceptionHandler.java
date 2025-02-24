292c5681f561702ed9c82fbbedaefa830238dbc1
Михаил Шелудяков <bvg2003@bk.ru>
2025-02-24 21:35:50 +0300

    ExceptionHandler fix

--- src/main/java/ru/michaelshell/taskmanager/exception/ApplicationExceptionHandler.java
+++ src/main/java/ru/michaelshell/taskmanager/exception/ApplicationExceptionHandler.java
@@ -1,6 +1,5 @@
 package ru.michaelshell.taskmanager.exception;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,22 +16,21 @@ public class ApplicationExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(NOT_FOUND)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
-        return ResponseEntity.status(NOT_FOUND).body(e.getMessage());
+    public String handleResourceNotFoundException(ResourceNotFoundException e) {
+        return e.getMessage();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(BAD_REQUEST)
-    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        String message = e.getBindingResult().getFieldErrors().stream()
+    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getFieldErrors().stream()
                 .map(error -> error.getField() + " " + error.getDefaultMessage())
                 .collect(Collectors.joining("\n"));
-        return ResponseEntity.badRequest().body(message);
     }
 
     @ExceptionHandler(HttpMessageConversionException.class)
     @ResponseStatus(BAD_REQUEST)
-    public ResponseEntity<String> handleHttpMessageConversionException(HttpMessageConversionException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public String handleHttpMessageConversionException(HttpMessageConversionException e) {
+        return e.getMessage();
     }
 }
